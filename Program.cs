1a7f3a2cfc0f0a83ad12d86d7b546a5137822480
Beltran, Yndira <Yndira.Beltran@training.lug-ag.de>
2025-10-16 17:46:34 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using OnlineShopping;
+//using System.Text.Json;
+
+
+//const string employeesFile = "Employees.json";
+
+//List<Employee> Employees = ReadEmployeesFile();
+
+
+DisplayDivisions();
+
+void DisplayDivisions()
+{
+    Console.WriteLine("***** You welcome to Dora Lux *****\n +" +
+                      "***** We illuminate your life with LEDs *****");
+}
