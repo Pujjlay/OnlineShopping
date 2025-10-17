a68d3df6faf75b3ab3e7d6ddedfd524b623fc9fb
Beltran, Yndira <Yndira.Beltran@training.lug-ag.de>
2025-10-17 17:25:31 +0200

    Menue implementiert

--- Program.cs
+++ Program.cs
@@ -2,15 +2,62 @@
 //using System.Text.Json;
 
 
-//const string employeesFile = "Employees.json";
+//const string factoryFile = "Factory.json";
 
-//List<Employee> Employees = ReadEmployeesFile();
+//List<Divisions> Factory = ReadFactoryFile();
 
 
 DisplayDivisions();
 
 void DisplayDivisions()
 {
-    Console.WriteLine("***** You welcome to Dora Lux *****\n +" +
-                      "***** We illuminate your life with LEDs *****");
+    bool running = true;
+
+    while (running)
+    {
+        Console.WriteLine("******** You welcome to Dora Lux! *********\n**** We illuminate your life with LEDs ****\n\n " +
+                       "Which division do you want to enter?\n\n 1. Factory" +
+                          "\n 2. Home\n 3. Outside\n 4. Event" +
+                            "\n 5. Exit\n\n Please make your choice...\n ");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                FactoryDivision();
+                break;
+
+            case 2:
+                //HomeDivision();
+                break;
+            case 3:
+                //OutsideDivision();
+                break;
+            case 4:
+                //EventDivision();
+                break;
+            case 5:
+                running = false;
+                Console.WriteLine("\nThanks for your collaboration! See you!");
+                break;
+            default:
+                Console.WriteLine("Invalid input");
+                break;
+
+        }
+    }
+
+
+}
+
+void FactoryDivision()
+{
+    Console.Clear();
+    Console.WriteLine($"Enter the item number: ");
+    var ItemNum = Console.ReadLine();
+    Console.WriteLine($"Enter the name of the good: ");
+    var Name = Console.ReadLine();
+    Console.WriteLine($"Enter the price in euro: ");
+    var Price = Console.ReadLine();
+
 }
